d71458107e037f004796f7239d071d3ec407b0e2
Dave Hall <david.hall.richmond@gmail.com>
2025-05-23 11:42:58 -0400

    adding back in stubbed environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  openaiApiKey: 'your-openai-api-key',
+  pineconeApiKey: 'your-pinecone-api-key',
+  pineconeEnvironment: 'your-pinecone-environment',
+  pineconeIndex: 'your-pinecone-index',
+  awsRegion: 'your-aws-region',
+  awsAccessKeyId: 'your-aws-access-key-id',
+  awsSecretAccessKey: 'your-aws-secret-access-key',
+  dynamoDbTable: 'recipes'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  pineconeApiUrl: '',
+  openaiApiKey: '',
+  myAwsRegion: '',
+  myAwsAccessKeyId: '',
+  myAwsSecretAccessKey: '',
+  dynamoDbTable: ''
+};
